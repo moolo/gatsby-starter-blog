e8a928f73daeea58ba146f6891d23149adef3803
moolo <wingx@126.com>
2018-10-14 03:01:50 +0800

    npm start

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Install this starter (assuming Gatsby is installed) by running from your CLI:
 `gatsby new gatsby-blog https://github.com/gatsbyjs/gatsby-starter-blog`
 
 ## Running in development
-`gatsby develop`
+`npm start`
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ module.exports = {
     title: 'Gatsby Starter Blog',
     author: 'Kyle Mathews',
     description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://moolo.github.io/blog/',
+    siteUrl: 'https://moolo.github.io/gatsby-starter-blog/',
   },
-  pathPrefix: '/blog',
+  pathPrefix: '/gatsby-starter-blog',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
